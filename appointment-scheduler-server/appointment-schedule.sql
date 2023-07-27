ffd37cf50dcc94c6391af4c61c9cf3911cd19b66
Yuvia Leon <ykl2112@columbia.edu>
2023-07-27 17:46:04 -0500

    added sql scripts and user table

---
+++ appointment-scheduler-server/appointment-schedule.sql
@@ -0,0 +1,9 @@
+\echo 'Delete and recreate appointment-scheduler db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE appointment_scheduler_db;
+CREATE DATABASE appointment_scheduler_db;
+\connect appointment_scheduler_db;
+
+-- run script 
+\i appointment-scheduler-schema.sql 
