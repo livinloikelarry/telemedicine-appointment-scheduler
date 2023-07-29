1673e35ec85fa2411431b09f4bee02171a8b54d2
Yuvia Leon <ykl2112@columbia.edu>
2023-07-29 16:22:46 -0500

    installed jwt token

---
+++ appointment-scheduler-server/utils/tokens.js
@@ -0,0 +1,28 @@
+// store all of our token related functions
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+const createUserJwt = (user) => {
+  const payload = {
+    email: user.email,
+  };
+  return generateToken(payload);
+};
+
+const generateToken = (data) =>
+  jwt.sign(data, SECRET_KEY, { expiresIn: "24h" });
+const validatedToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    return decoded;
+  } catch (err) {
+    return {};
+  }
+};
+
+module.exports = {
+  generateToken,
+  createUserJwt,
+  validatedToken,
+};
